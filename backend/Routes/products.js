39bb5f6aa0e05d461906205bee7882978edf8c5e
Davidhzk2 <dherrera035@misena.edu.co>
2021-05-27 16:23:42 -0500

    Editar y eliminar un producto

--- backend/Routes/products.js
+++ backend/Routes/products.js
@@ -37,5 +37,38 @@ router.get("/listProducts/", Auth, async (req,res)=>{
     return res.status(200).send({products});
 });
 
+// actualizar los productos
+router.put("/updateProduct/", Auth, async (req, res) =>{
+    // buscar el usuario por id
+    const user = await User.findById(req.user._id);
+    if(!user) return res.status(400).send("no se encontro usuario en la bd");
+
+    // buscar y capturar los datos 
+    const product = await Products.findByIdAndUpdate(req.body._id ,{
+        userId: user._id,
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.category,
+        valor: req.body.valor
+    });
+    // validar el producto
+    if(!product) return res.status(400).send("No se pudo editar el producto");
+    return res.status(200).send({product});
+});
+
+
+// Eliminar producto 
+router.delete("/:_id", Auth, async (req, res) =>{
+    //consultar usuario por id 
+    const user = await User.findById(req.user._id);
+    // validar usuario 
+    if(!user) return res.status(400).send("no se encontro usuario en la bd");
+    // buscar y eliminar producto por id
+    const product = await Products.findByIdAndDelete(req.params._id);
+    // validar producto
+    if(!product) return res.status().send("Error: no se pudo eliminr el producto");
+    return res.status(200).send("El prodcuto se elimino con exito!");
+});
+
 // exportar controlador
 module.exports = router;
