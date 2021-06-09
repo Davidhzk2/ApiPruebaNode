257ec7dabd773ab7996806e13673766b6740b33d
Davidhzk2 <dherrera035@misena.edu.co>
2021-06-09 18:28:03 -0500

    Rutas del proyecto en el header

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from "./home/login/login.component";
+import { RegisterComponent } from "./home/register/register.component";
+import { RegisterProductComponent } from "./product/register-product/register-product.component";
+import { ListProductComponent } from "./product/list-product/list-product.component";
+import { UpdateProductComponent } from "./product/update-product/update-product.component";
+
+const routes: Routes = [
+  {
+    path:'',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent,
+  },
+  { 
+    path:'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'registerProduct',
+    component:  RegisterProductComponent
+  },
+  {
+    path: 'listProduct',
+    component:  ListProductComponent
+  },
+  {
+    path: 'updateProduct',
+    component:  UpdateProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
