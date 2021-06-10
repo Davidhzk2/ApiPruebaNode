a99b3cded44f2d06e1fcbdaed0f98d23da7a9a2b
Davidhzk2 <dherrera035@misena.edu.co>
2021-06-10 18:10:54 -0500

    Componente para registrar usuario con los modulos necesarios

--- Frontend/src/app/home/register/register.component.ts
+++ Frontend/src/app/home/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public registerData : any;
+  public successMessage: String;
+  public errorMessage: String;
 
-  constructor() { }
+  constructor(private auth:AuthService, private router: Router) { 
+    this.registerData = {};
+    this.successMessage = '';
+    this.errorMessage = '';
+  }
 
   ngOnInit(): void {
+  }
+
+  registerUser(){
+    if(!this.registerData.name || !this.registerData.username || !this.registerData.password){
+        this.errorMessage = "Failed : data incomplete!";
+        this.closeAlert();
+        this.registerData = {}
+    }else{
+      this.auth.registerUser(this.registerData).subscribe(
+        (res) =>{
+          console.log(res);
+          this.successMessage = 'Successfuly: Register user';
+          this.closeAlert();
+          this.registerData = {}
+        }, 
+        (err) =>{
+          console.log(err);
+          this.errorMessage = err.error;
+          this.closeAlert();
+          this.registerData = {}
+        }
+      );
+    }
+  };
+
+  closeAlert(){
+    setTimeout(() => {
+      this.successMessage ='';
+      this.errorMessage = '';
+    }, 3000);
+  }
+  close(){
+      this.successMessage ='';
+      this.errorMessage = '';
   }
 
 }
